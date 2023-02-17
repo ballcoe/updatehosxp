a6c5d603f5d00faad3b51d8847cd1f88371d3efc
pitchapong polyiam <42347032+ballcoe@users.noreply.github.com>
2023-02-17 21:50:56 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$conn = new mysqli("localhost", "username", "password" , "database");
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
